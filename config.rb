40c4db0cc2b3fc9bf31381ccb5a508c9f02d8485
Cameron Spear <cameronspear@gmail.com>
2013-07-03 16:14:05 -0700

    Change file paths after changing package name.

--- config.rb
+++ config.rb
@@ -2,11 +2,11 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "Hangouts.AdiumMessageStyle/Contents/Resources/"
+css_dir = "Google Hangouts.AdiumMessageStyle/Contents/Resources/"
 sass_dir = "scss"
 images_dir = "img"
-javascripts_dir = "Hangouts.AdiumMessageStyle/Contents/Resources/js"
-fonts_dir = "Hangouts.AdiumMessageStyle/Contents/Resources/fonts"
+javascripts_dir = "Google Hangouts.AdiumMessageStyle/Contents/Resources/js"
+fonts_dir = "Google Hangouts.AdiumMessageStyle/Contents/Resources/fonts"
 
 output_style = :compressed
 
